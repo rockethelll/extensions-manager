36c5dae2627dc25fdd9ef181d4bc2a21b8af5293
rockethelll <rockethell@hotmail.com>
2025-07-01 15:02:15 +0200

    Update Jest setup configuration and modify GitHub Actions deploy message format

--- setup-jest.ts
+++ setup-jest.ts
@@ -1,5 +1,5 @@
-// import 'jest-preset-angular/setup-jest';
+import 'jest-preset-angular/setup-jest';
 
-import { setupZoneTestEnv } from 'jest-preset-angular/setup-env/zone';
-setupZoneTestEnv();
+// import { setupZoneTestEnv } from 'jest-preset-angular/setup-env/zone';
+// setupZoneTestEnv();
 import '@testing-library/jest-dom';
